a5b02f058e298d566a6d95e1e89edcc31bfe22fb
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-09-27 16:01:19 +0200

    proves

--- script/index.js
+++ script/index.js
@@ -1,8 +1,5 @@
 var amplada;
 var alsada;
 
-amplada = document.getElementById("spanAmplada").textContent;
-alsada = document.getElementById("spanAlsada").textContent;
-
-alert(amplada);
-alert(alsada);
+amplada = document.getElementById("spanAmplada");
+alsada = document.getElementById("spanAlsada");
