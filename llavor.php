d35d516c258b5f8ec52781063f3711a705ddf6f8
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-09-27 10:45:13 +0200

    proves

--- llavor.php
+++ llavor.php
@@ -20,11 +20,20 @@
         <h2>Selecciona les Caselles Vives</h2>
         <form action="" method="POST">
             <table id="tauler">
-                <?php 
-                    echo $amplada;
-                    echo $alsada;
+                <script>
+                    var amplada = <?=$amplada?>;
+                    var alsada = <?=$alsada?>;
 
-                ?>
+                    var tr = document.createElement("tr");
+
+                    var td1 = document.createElement("td");
+                    var td2 = document.createElement("td");
+
+                    tr.appendChild(td1);
+                    tr.appendChild(td2);
+
+                    document.body.div.form.table.appendChild(tr);
+                </script>
             </table>
             <input type="submit" name="jugar" value="Jugar" class="boto">
         </form>
