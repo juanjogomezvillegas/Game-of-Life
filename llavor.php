15968a65f613082f18b30213422b0d0b65cb629b
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-09-27 16:38:03 +0200

    proves

--- llavor.php
+++ llavor.php
@@ -14,8 +14,8 @@
         $alsada = $_POST["alsada"];
 
     ?>
-    <p id="pAmplada"><?=$amplada?></p>
-    <p id="pAlsada"><?=$alsada?></p>
+    <a id="pAmplada"><?=$amplada?></a>
+    <a id="pAlsada"><?=$alsada?></a>
     <header>
         <h1 class="titol">El Joc de la Vida</h1>
     </header>
