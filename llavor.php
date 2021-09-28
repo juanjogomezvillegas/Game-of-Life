5230e66eb6e1a830e8d2398643d22513c826aff6
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-09-28 18:26:04 +0200

    proves

--- llavor.php
+++ llavor.php
@@ -26,7 +26,7 @@
                     <tr>
                         <?php for ($j = 1; $j < $amplada; $j++) { ?>
                             <?php $nomCela = "Cela" . $comptador; ?>
-                            <td><input type="checkbox" name="Celes[]" id="<?=$nomCela?>" value="<?=$comptador?>" class="celes"></td>
+                            <td><input type="checkbox" name="Celes[]" id="<?=$nomCela?>" class="celes" value="<?=$comptador?>"></td>
                             <?php $comptador++; ?>
                         <?php } ?>
                     </tr>
