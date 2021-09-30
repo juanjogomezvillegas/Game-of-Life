461cada571d7f21fbb2e415f11e3ad0b0fb48902
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-09-30 15:27:00 +0200

    proves

--- partida.php
+++ partida.php
@@ -47,11 +47,9 @@
                     
                     var arrayCelesVives = varCelesVives.split(" ");
 
-                    document.write(<?=$varCeles?>);
-
-                    /*for (var i = 0; i < arrayCelesVives.length; i++) {
+                    for (var i = 0; i < arrayCelesVives.length; i++) {
                         document.write(arrayCelesVives[i] + " ");
-                    }*/
+                    }
 
                     /*for (var i = 1; i <= alsada; i++) {
                         var fila = document.createElement("tr");
