b8ee71d84e9b7e19de99a548f31f7a11b99aec69
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-10-01 15:02:51 +0200

    Creacio del Funcionament del Joc

--- script/tauler.js
+++ script/tauler.js
@@ -1,6 +1,12 @@
 var arrel = document.getElementById("tauler");
 
-var celes = arrel.getElementsByTagName("td").innerHTML;
+var files = arrel.getElementsByTagName("tr");
+
+var celes;
+for (var i = 0; i < files.length; i++) {
+    celes = files[i].getElementsByTagName("td").innerHTML;
+    
+}
 
 for (var i = 0; i < celes.length; i++) {
     alert(celes[i]);
