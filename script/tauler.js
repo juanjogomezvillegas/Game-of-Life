2033707c9579b129ae0dd51481c6707ae9c34483
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-10-02 12:58:52 +0200

    prova

--- script/tauler.js
+++ script/tauler.js
@@ -1,3 +1,8 @@
+var tauler;
+var files;
+var celes;
+var celesTauler;
+
 window.onload = function() {
     var amplada = document.getElementById("ampladaTauler").innerHTML;
     var alsada = document.getElementById("alsadaTauler").innerHTML;
@@ -6,13 +11,11 @@ window.onload = function() {
     var numGeneracio = 0;
     generacio.innerHTML = "Generació: "+numGeneracio;
 
-    var tauler = document.getElementById("tauler");
+    tauler = document.getElementById("tauler");
 
-    var files = tauler.getElementsByTagName("tr");
+    files = tauler.getElementsByTagName("tr");
 
-    var celes;
-
-    var celesTauler = new Array(files.length);
+    celesTauler = new Array(files.length);
 
     for (var i = 0; i < files.length; i++) {
         celes = files[i].getElementsByTagName("td");
@@ -21,11 +24,11 @@ window.onload = function() {
 
     PintaTauler(celesTauler);
 
-    document.getElementById("bPlay").addEventListener("click", play(celesTauler));
+    document.getElementById("bPlay").addEventListener("click", play);
     document.getElementById("bPause").addEventListener("click", pause);
 };
 
-function play(celesTauler) {
+function play() {
     for (var i = 0; i < celesTauler.length; i++) {
         for (var j = 0; j < celesTauler[i].length; j++) {
             alert(celesTauler[i][j].innerHTML);
