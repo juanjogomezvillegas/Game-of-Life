08e74a9d1a21a4fb912bf3ce6f7e0302b289d934
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-10-04 17:00:53 +0200

    prova

--- script/tauler.js
+++ script/tauler.js
@@ -57,14 +57,26 @@ function CanvisTauler() {
         for (var j = 0; j < celesTauler[i].length; j++) {
             comptadorVeins = 0;
             for (var k = 0; k < celesTauler2.length; k++) {
-                for (var l = 0; l > 0 && l < celesTauler2[k].length; l++) {
-                    if (celesTauler[i][j] == celesTauler2[k][l-1] || celesTauler[i][j] == celesTauler2[k][l+1] || 
-                        celesTauler[i][j] == celesTauler2[k-1][l] || celesTauler[i][j] == celesTauler2[k+1][l] || 
-                        celesTauler[i][j] == celesTauler2[k-1][l-1] || celesTauler[i][j] == celesTauler2[k+1][l+1]) {
-                            if (celesTauler[i][j].innerHTML == "1") {
-                                comptadorVeins++;
+                if (k == 0) {
+                    for (var l = 1; l < celesTauler2[k].length; l++) {
+                        if (celesTauler[i][j] == celesTauler2[k][l-1] || celesTauler[i][j] == celesTauler2[k][l+1] || 
+                            celesTauler[i][j] == celesTauler2[k-1][l] || celesTauler[i][j] == celesTauler2[k+1][l] || 
+                            celesTauler[i][j] == celesTauler2[k-1][l-1] || celesTauler[i][j] == celesTauler2[k+1][l+1]) {
+                                if (celesTauler[i][j].innerHTML == "1") {
+                                    comptadorVeins++;
+                                }
                             }
-                        }
+                    }
+                } else {
+                    for (var l = 0; l < celesTauler2[k].length; l++) {
+                        if (celesTauler[i][j] == celesTauler2[k][l-1] || celesTauler[i][j] == celesTauler2[k][l+1] || 
+                            celesTauler[i][j] == celesTauler2[k-1][l] || celesTauler[i][j] == celesTauler2[k+1][l] || 
+                            celesTauler[i][j] == celesTauler2[k-1][l-1] || celesTauler[i][j] == celesTauler2[k+1][l+1]) {
+                                if (celesTauler[i][j].innerHTML == "1") {
+                                    comptadorVeins++;
+                                }
+                            }
+                    }
                 }
             }
             if (celesTauler[i][j].innerHTML == "1") {
