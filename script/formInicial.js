4aa8713d7696b66385254bee39c0389182ac06db
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-10-05 10:10:26 +0200

    prova

--- script/formInicial.js
+++ script/formInicial.js
@@ -1,12 +1,9 @@
-var form;
 var amplada;
 var alsada;
 var indexUrl;
 var llavorUrl;
 
 window.onload = function() {
-    form = document.getElementById("formulariInicial");
-
     document.getElementById("botoSeguent").addEventListener("click", validarFormulari);
 };
 
@@ -14,11 +11,9 @@ function validarFormulari() {
     alsada = document.getElementById("inputAlsada");
     amplada = document.getElementById("inputAmplada");
 
-
-    console.log(form);
     if ((amplada == "" || amplada == " ") && (alsada == "" || alsada == " ")) {
-        form.setAttribute("action", "index.html");
+        document.getElementById("formulariInicial").setAttribute("action", "index.html");
     } else {
-        form.setAttribute("action", "llavor.php");
+        document.getElementById("formulariInicial").setAttribute("action", "llavor.php");
     }
 };
