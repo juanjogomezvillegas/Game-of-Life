b60d48e73139d27123a2b156ee9fcdc0c3ac01bb
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-10-05 20:40:39 +0200

    prova

--- partida.php
+++ partida.php
@@ -43,7 +43,7 @@
             <div id="inputs">
                 <input type="button" name="play" value="Play" class="boto" id="bPlay">
                 <input type="button" name="pause" value="Pause" class="boto" id="bPause">
-                <label for="inputvelocitat" id="labelvelocitat">Velocitat<br><input type="range" name="velocitat" min="100" max="3000" default="1000" id="inputvelocitat"></label>
+                <label for="inputvelocitat" id="labelvelocitat">Velocitat<br><input type="range" name="velocitat" min="100" max="1000" default="500" id="inputvelocitat"></label>
             </div>
         </form>
     </div>
