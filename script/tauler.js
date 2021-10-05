68356b084c7bc5026243bd1ff0a8e835b6c67037
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-10-05 18:53:47 +0200

    prova

--- script/tauler.js
+++ script/tauler.js
@@ -117,9 +117,9 @@ function setCanvisTauler() {
             }
 
             if (celesTauler2[i][j].innerHTML == "1") {
-                if (comptadorVeins < 2) {
+                if (comptadorVeins <= 1) {
                     celesTauler[i][j].innerHTML = "0";
-                } else if (comptadorVeins > 3) {
+                } else if (comptadorVeins >= 4) {
                     celesTauler[i][j].innerHTML = "0";
                 } else if (comptadorVeins >= 2 && comptadorVeins <= 3) {
                     celesTauler[i][j].innerHTML = "1";
