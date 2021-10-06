14196c9d2c47a68187108d034b1a428d5300301a
juanjogomezvillegas <juanjogomvil@gmail.com>
2021-10-06 15:42:12 +0200

    proves

--- llavor.php
+++ llavor.php
@@ -48,9 +48,9 @@
                     <tr>
                         <?php for ($j = 1; $j <= $amplada; $j++) { ?>
                             <?php if (isset($celesVives[$i][$j])) { ?>
-                                <td class="celesLlavor"><input type="checkbox" name="tauler[<?=$i?>][<?=$j?>]" value="<?=$celesVives[$i][$j]?>"></td>
+                                <td class="celesLlavor"><input type="checkbox" name="tauler[<?=$i?>][<?=$j?>]" value="on"></td>
                             <?php } else { ?>
-                                <td class="celesLlavor"><input type="checkbox" name="tauler[<?=$i?>][<?=$j?>]" value="<?=$celesVives[$i][$j]?>"></td>
+                                <td class="celesLlavor"><input type="checkbox" name="tauler[<?=$i?>][<?=$j?>]" value=""></td>
                             <?php } ?>
                         <?php } ?>
                     </tr>
